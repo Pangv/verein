1f53fda24638060e46e06c0738a65341ec51af78
Steve Biffer <sven.oliverpaetzel@gmail.com>
2016-12-03 22:18:40 +0100

    Initial netbeans commit

---
+++ verein/src/de/lebk/verein/entry/Entry.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.lebk.verein.entry;
+
+/**
+ *
+ * @author sopaetzel
+ */
+public class Entry {
+    
+    public static void main(String[] args) {
+        System.out.println("It is alive!");
+    }
+    
+}
