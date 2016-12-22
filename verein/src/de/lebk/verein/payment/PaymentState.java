ed461b56bc8b6e13608f69128f2fccf58f8266f9
Marcel Raddatz <raddatz@gates-services.de>
2016-12-22 12:24:49 +0100

    Added PaymentStateEnum

---
+++ verein/src/de/lebk/verein/payment/PaymentState.java
@@ -0,0 +1,26 @@
+package de.lebk.verein.payment;
+
+/**
+ * Created by marcel on 22.12.16.
+ */
+public enum PaymentState {
+	PAID("paid"),
+	OPEN("open");
+
+	private final String text;
+
+	PaymentState(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	/* (non-Javadoc)
+		 * @see java.lang.Enum#toString()
+		 */
+	@Override public String toString() {
+		return text;
+	}
+}
