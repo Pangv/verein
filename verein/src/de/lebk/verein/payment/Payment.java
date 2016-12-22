a09b549d2ca5dca2da3b031671111f1a6c668daf
Marcel Raddatz <raddatz@gates-services.de>
2016-12-22 12:26:09 +0100

    implemented Payment

--- verein/src/de/lebk/verein/payment/Payment.java
+++ verein/src/de/lebk/verein/payment/Payment.java
@@ -23,10 +23,44 @@
  */
 package de.lebk.verein.payment;
 
+import de.lebk.verein.member.Member;
+
 /**
  *
  * @author Sven-Oliver Pätzel
  */
 public class Payment {
-    
+	private Member member;
+	private PaymentState state;
+	private double amount;
+
+	public Payment(Member member, double amount) {
+		this.member = member;
+		this.amount = amount;
+		this.state = PaymentState.OPEN;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public PaymentState getState() {
+		return state;
+	}
+
+	public void setState(PaymentState state) {
+		this.state = state;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 }
