ac2cd9544c7257a25eb54d09cc5333b70e2bcf69
Marcel Raddatz <raddatz@gates-services.de>
2017-01-26 11:39:55 +0100

    removed if clause in favor of throwing an exception

--- verein/src/de/lebk/verein/login/Auth.java
+++ verein/src/de/lebk/verein/login/Auth.java
@@ -6,52 +6,48 @@ import de.lebk.verein.member.Officer;
 import de.lebk.verein.member.Role;
 
 /**
- *
  * @author raddatz
  */
 public class Auth {
 
-    private Role role;
-    private Club club;
-    private Member user = null;
-    private Member currentUser = null;
+	private Role role;
+	private Club club;
+	private Member user = null;
+	private Member currentUser = null;
 
-    private static Auth ourInstance = new Auth();
+	private static Auth ourInstance = new Auth();
 
-    public static Auth getInstance() {
-        return ourInstance;
-    }
+	public static Auth getInstance() {
+		return ourInstance;
+	}
 
-    private Auth() {
-    }
+	private Auth() {
+	}
 
-    public void logout() {
-        this.currentUser = null;
-    }
+	public void logout() {
+		this.currentUser = null;
+	}
 
 	public boolean login(String username, String password)
 		throws UserNotFoundException, WrongPasswordException {
 		Member possibleUser = club.getUser(username);
-		if (possibleUser != null) {
-			if (possibleUser.getPassword().equals(password)) {
-               this.currentUser = possibleUser;
-               this.role = Role.valueOf(currentUser.getClass().getSimpleName().toUpperCase());
-               return true;
-           }else {
-               throw new WrongPasswordException();
-           }
-       }else {
-           throw new UserNotFoundException();
-       }
-    }
+		if (possibleUser.getPassword().equals(password)) {
+			this.currentUser = possibleUser;
+			this.role = Role.valueOf(currentUser.getClass().getSimpleName().toUpperCase());
+			return true;
+		} else {
+			throw new WrongPasswordException();
+		}
 
-    public Role getRole() {
-        return role;
-    }
+	}
 
-    public Member getCurrentUser() {
-        return currentUser;
-    }
+	public Role getRole() {
+		return role;
+	}
+
+	public Member getCurrentUser() {
+		return currentUser;
+	}
 
 	public void setClub(Club club) {
 		this.club = club;
