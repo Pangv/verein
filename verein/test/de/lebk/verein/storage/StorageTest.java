f64a3aa74b3815377aef68d7e3106de56faa3ec2
Eva Brinker <brinker@picture-gmbh.de>
2017-01-27 10:30:31 +0100

    added final storage test

--- verein/test/de/lebk/verein/storage/StorageTest.java
+++ verein/test/de/lebk/verein/storage/StorageTest.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.junit.Test;
 import java.util.GregorianCalendar;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Eva Brinker
@@ -74,12 +74,38 @@ public class StorageTest {
         
         assertEquals("there should be 2 entries in overdue leases because there "
                 + "are 2 members with overdue leases", 2, overdueLeases.size());
-
+        assertTrue("one of the member with overdue leases should be member1", 
+                overdueLeases.keySet().stream().anyMatch(member -> member.getFirstName().equals("member1")));
+        assertTrue("one of the member with overdue leases should be member2", 
+                overdueLeases.keySet().stream().anyMatch(member -> member.getFirstName().equals("member2")));
+        assertEquals("member1 should have 1 overdue lease", 1, overdueLeases.get(member1).size());
+        assertEquals("member2 should have 2 overdue lease", 2, overdueLeases.get(member2).size());
     }
 
     @Test
     public void testGetOverdueLeasesForMember() {
-
+        Storage storage = new Storage();
+       
+        Member member = new Member();
+        
+        GregorianCalendar today = new GregorianCalendar();
+        today.set(2017, 2, 4);
+        storage.setToday(today);
+        
+        GregorianCalendar dateBeforeToday = new GregorianCalendar();
+        dateBeforeToday.set(2017, 1, 1);
+        
+        GregorianCalendar dateAfterToday = new GregorianCalendar();
+        dateAfterToday.set(2017, 4, 2);
+        
+        storage.addLease(member, 15, dateBeforeToday);
+        storage.addLease(member, 22, dateBeforeToday);
+        storage.addLease(member, 11, dateAfterToday);
+        
+        List<Lease> overdueLeases = storage.getOverdueLeasesForMember(member);
+        
+        assertEquals("there should be 2 overdue leases because the member has "
+                + "2 overdue leases", 2, overdueLeases.size());
     }
 
 }
